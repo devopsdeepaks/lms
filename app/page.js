12b466a9ca99f59cf1d9c72175bc5260fce50be9
devopsdeepaks <deepaksinghalgpg@gmail.com>
2025-03-07 20:43:28 +0530

    initialize requirements

--- app/page.js
+++ app/page.js
@@ -1,13 +1,13 @@
+"use client";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <h1>hiiiii</h1>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    
+      <Button variant="ghost">Click me</Button>
+      <Image src="/logo.svg" alt="logo" width={200} height={200} />
     </div>
   );
 }
